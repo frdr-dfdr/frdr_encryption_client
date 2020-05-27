68895d848ab1274c2c845185306580134bebda1a
jza201 <jza201@sfu.ca>
2020-05-27 15:47:30 -0700

    remove create policy and group methods in AccessGranter

--- util/AccessGranter.py
+++ util/AccessGranter.py
@@ -4,32 +4,8 @@ class AccessGranter(object):
         self._vault_client = vault_client
         self._depositor_entity_id = self._vault_client.entity_id
     def grant_access(self, requester_entity_id, dataset_id):
-        policy_name = self._create_share_policy(dataset_id)
-        group_name = self._create_share_group(dataset_id, policy_name)
-        self._add_member_to_group(group_name, requester_entity_id)
-        
-    def _create_share_policy(self, dataset_id):
-        policy_string = """
-            path "secret/data/{user_entity_id}/{dataset_id}" {{
-                capabilities = [ "read", "list", "delete"]
-            }}
-        """.format(user_entity_id=self._depositor_entity_id, dataset_id=dataset_id)
-        policy_name = "_".join((self._depositor_entity_id, dataset_id, "share_policy"))
-        if self._vault_client.create_policy(policy_name, policy_string):
-            return policy_name
-        else:
-            #TODO: raise error or return false
-            raise RuntimeError
-        
-    def _create_share_group(self, dataset_id, policy_name):
         group_name = "_".join((self._depositor_entity_id, dataset_id, "share_group"))
-        response = self._vault_client.create_or_update_group_by_name(group_name=group_name,
-                                                                     policy_name=policy_name)
-        try:
-            group_name = response["data"]["name"]
-        except:
-            pass
-        return group_name
+        self._add_member_to_group(group_name, requester_entity_id)
     
     def _add_member_to_group(self, group_name, requester_entity_id):
         read_group_response = self._vault_client.read_group_by_name(group_name)
@@ -38,6 +14,3 @@ class AccessGranter(object):
         policies = read_group_response["data"]["policies"]
         self._vault_client.create_or_update_group_by_name(group_name, policies, member_entity_ids)
     
-    def _add_policy_to_group(self, group_name, policy_name):
-        self._vault_client.create_or_update_group_by_name(group_name=group_name, 
-                                                          policy_name=policy_name)
