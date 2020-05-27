b39cb1dc21b69cc50cb3a3314c986c4231f4892a
jza201 <jza201@sfu.ca>
2020-05-27 15:49:19 -0700

    add create policy and group methods to the KeyManagementVault class

--- util/KeyGenerator.py
+++ util/KeyGenerator.py
@@ -29,6 +29,8 @@ class KeyManagementVault(object):
         self._key_ring_name = key_ring_name #key_ring_name is the dataset_id
         self._key = None
         self._key_ciphertext = None
+        self._dataset_access_policy_name = "_".join((self._vault_client.entity_id, self._key_ring_name, "share_policy"))
+        self._dataset_access_group_name = "_".join((self._vault_client.entity_id, self._key_ring_name, "share_group"))
 
     def generate_key(self):
         # enable transit engine should be done by vault admin
@@ -51,6 +53,54 @@ class KeyManagementVault(object):
 
     def get_vault_entity_id(self):
         return self._vault_client.entity_id
+
+    def create_access_policy_and_group(self):
+        if not self._dataset_access_policy_exists():
+            self._create_dataset_access_policy()
+        if not self._dataset_access_group_exists():
+            self._create_dataset_access_group()
+
+    def _dataset_access_policy_exists(self):
+        try:
+            self._vault_client.read_policy(self._dataset_access_policy_name)
+        except hvac.exceptions.InvalidPath:
+            print ("Policy does not exist.")
+            return False
+        else:
+            print ("Policy already exists.")
+            return False
+
+    def _create_dataset_access_policy(self):
+        policy_string = """
+            path "secret/data/{user_entity_id}/{dataset_id}" {{
+                capabilities = [ "read", "list", "delete"]
+            }}
+        """.format(user_entity_id=self._vault_client.entity_id, dataset_id=self._key_ring_name)
+        if self._vault_client.create_policy(self._dataset_access_policy_name, policy_string):
+            return True
+        else:
+            #TODO: raise error or return false
+            raise RuntimeError
+
+    def _dataset_access_group_exists(self):
+        try:
+            self._vault_client.read_group_by_name(self._dataset_access_group_name)
+        except hvac.exceptions.InvalidPath:
+            print ("Group does not exist")
+            return False
+        print ("Group already exists.")
+        return True
+
+    def _create_dataset_access_group(self):
+        print ("create group")
+        response = self._vault_client.create_or_update_group_by_name(group_name=self._dataset_access_group_name,
+                                                                     policy_name=self._dataset_access_policy_name)
+        # TODO: check the return value
+        try:
+            group_name = response["data"]["name"]
+        except:
+            pass
+        return group_name
 
     @property
     def key(self):
