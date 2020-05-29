fa0570c3fb914513a1dcb68f627ec5af9d181071
jza201 <jza201@sfu.ca>
2020-05-28 23:46:43 -0700

    add set_logger function

--- util/util.py
+++ util/util.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import logging
 from base64 import b64encode, b64decode
 class Util(object):
     
@@ -21,3 +23,45 @@ class Util(object):
             return path[:-1]
         else:
             return path
+
+    @classmethod
+    def get_logger(cls, name, log_level="INFO", filepath=None):
+        logger = logging.getLogger(name)
+        # if logger already exists, return it
+        if logger.handlers:
+            return logger
+        
+        level = getattr(logging, log_level.upper() if log_level else "INFO")
+        try:
+            logger.setLevel(level)
+        except: 
+            logger.setLevel(logging.INFO)
+
+        logger.info("Log level: {}".format(log_level))
+
+        clean_logformatter = '%(message)s'
+        clean_formatter = logging.Formatter(clean_logformatter)
+        console = logging.StreamHandler(sys.stdout)
+        try:
+            console.setLevel(level)
+        except: 
+            console.setLevel(logging.INFO)
+
+        # console.addFilter(lambda record: record.levelno <= logging.INFO)
+        console.setFormatter(clean_formatter)
+        logger.addHandler(console)
+        
+        # Create handlers
+        if filepath:
+            logformatter = '%(asctime)s [%(levelname)s] %(message)s'
+            formatter = logging.Formatter(logformatter, '%Y-%m-%d %H:%M:%S')
+            file_handler = logging.FileHandler(filepath, encoding='utf-8')
+            try:
+                file_handler.setLevel(level)
+            except:
+                file_handler.setLevel(logging.INFO)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+
+        return logger
