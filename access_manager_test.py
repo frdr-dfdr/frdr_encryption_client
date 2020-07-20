6d21bd2b5f8a29afc6ad580cb871caee60ece827
jza201 <jza201@sfu.ca>
2020-07-20 14:21:41 -0700

    rename variable

--- access_manager_test.py
+++ access_manager_test.py
@@ -32,9 +32,9 @@ if __name__ == "__main__":
                                vault_username=arguments["--username"], 
                                vault_passowrd=arguments["--password"], 
                                tokenfile=tokenfile)
-    access_granter = AccessManager(vault_client)
+    access_manager = AccessManager(vault_client)
     if arguments["--mode"] == "review-shares":
-        print (access_granter.list_members())
+        print (access_manager.list_members())
     else:
         requester_name = vault_client.read_entity_by_id(arguments["--requester"])
         if arguments["--mode"] == "grant-access":
@@ -42,10 +42,10 @@ if __name__ == "__main__":
                             .format(requester_name=requester_name, dataset_id=arguments["--name"])
             print (Util.wrap_text(warning_string))
             if click.confirm("Do you want to continue?", default=False):
-                access_granter.grant_access(arguments["--requester"], arguments["--name"])  
+                access_manager.grant_access(arguments["--requester"], arguments["--name"])  
         elif arguments["--mode"] == "revoke-access":
             warning_string = "You are trying to revoke requester {requester_name} access to dataset {dataset_id}"\
                             .format(requester_name=requester_name, dataset_id=arguments["--name"])
             print (Util.wrap_text(warning_string))
             if click.confirm("Do you want to continue?", default=False):
-                access_granter.revoke_access(arguments["--requester"], arguments["--name"])  
+                access_manager.revoke_access(arguments["--requester"], arguments["--name"])  
