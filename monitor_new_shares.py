bb92d28679422ad424f66c39060b144e7b2fec34
jza201 <jza201@sfu.ca>
2020-11-19 12:40:48 -0800

    remove unused method

--- monitor_new_shares.py
+++ monitor_new_shares.py
@@ -18,18 +18,6 @@ from modules.AccessManager import AccessManager
 
 __version__ = constants.VERSION
 
-def send_email(to_addrs, msg_subject, msg_body, from_addr=None):
-    msg = EmailMessage()
-    msg.set_content(msg_body)
-    if from_addr is not None:
-        msg['From'] = from_addr
-    msg['To'] = to_addrs
-    msg['Subject'] = msg_subject
-
-    sendmail_location = "/usr/sbin/sendmail"
-    subprocess.run([sendmail_location, "-t", "-oi"], input=msg.as_bytes())
-
-
 if __name__ == "__main__":
     arguments = docopt(__doc__, version=__version__)
     with open(arguments["--tokenfile"]) as f:
