60404961f0c38b7e8c87b226a564cb99fa1b4fdb
jza201 <jza201@sfu.ca>
2021-10-04 10:06:06 -0700

    RDM-5128 fix locale disconnect for contact us tab

--- gui/main_process/contactus.js
+++ gui/main_process/contactus.js
@@ -1,6 +1,6 @@
 const {ipcMain, BrowserWindow} = require('electron');
  
-ipcMain.on('contact-us', (event) => {
+ipcMain.on('contact-us', (event, locale) => {
   const win = new BrowserWindow({width: 800, height: 600});
-  win.loadURL("https://www.frdr-dfdr.ca/repo/contactus");  
+  win.loadURL("https://www.frdr-dfdr.ca/repo/contactus?locale=" + locale);  
 });
--- gui/renderer_process/contactus.js
+++ gui/renderer_process/contactus.js
@@ -1,7 +1,7 @@
 const {ipcRenderer} = require('electron');
 
 function contactus() {
-  ipcRenderer.send("contact-us");
+  ipcRenderer.send("contact-us", $.i18n().locale);
 }
 
 document.getElementById("contactus").addEventListener("click", contactus);
