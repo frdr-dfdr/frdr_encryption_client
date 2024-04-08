3844a9f7bd75f90bc2520a97b762973074ab1492
jza201 <jza201@sfu.ca>
2024-04-08 14:04:07 -0700

    RDM8137 fix file tree file format

--- modules/EncryptionClient.py
+++ modules/EncryptionClient.py
@@ -94,7 +94,7 @@ class EncryptionClient(object):
         
         # create a file tree file
         try:
-            with open(os.path.join(bag_dir, "file_tree.json"), "w") as f:
+            with open(os.path.join(bag_dir, "file_tree.txt"), "w") as f:
                 tree = Tree()
                 tree.totalSize += os.path.getsize(self._input)
                 print(self._input, file=f)
