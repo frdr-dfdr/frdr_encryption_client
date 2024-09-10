7cee747aacdd4cf6f6adffddfc7019253cec41f9
jza201 <jza201@sfu.ca>
2024-09-10 16:34:09 -0700

    Fix path in notarization script

--- codesign_scripts/notarize.js
+++ codesign_scripts/notarize.js
@@ -4,7 +4,7 @@ const { notarize } = require('@electron/notarize');
 async function packageTask () {
   // Package your app here, and code sign with hardened runtime
   await notarize({
-    appPath : "../gui/FRDR\ Encryption-darwin-x64/FRDR\ Encryption.app",
+    appPath : "../gui/dist/mac/FRDR\ Encryption.app",
     appBundleId: "ca.alliancecan.frdr.encrypt",
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
